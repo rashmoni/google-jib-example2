92693060f8b8c7d1e74f81e0cdd85d997a2d4770
Rashmoni Dey <91156734+rashmoni@users.noreply.github.com>
2024-07-03 14:57:30 +0200

    Update GoogleJibDemoApplication.java
    new update

--- src/main/java/com/example/googlejibdemo/GoogleJibDemoApplication.java
+++ src/main/java/com/example/googlejibdemo/GoogleJibDemoApplication.java
@@ -11,7 +11,7 @@ public class GoogleJibDemoApplication {
 
 	@GetMapping("/welcome")
 	public String Welcome(){
-		return "Welcome to jib";
+		return "Welcome to jib updated";
 	}
 
 	public static void main(String[] args) {
